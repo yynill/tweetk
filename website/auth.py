3c0338d40705e37a67fba02f2e1a2ff44c79d113
y <145238134+yynill@users.noreply.github.com>
2023-09-28 18:36:25 +0200

    login/logout

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -13,7 +14,10 @@ def sign_up():
         password = request.form.get('password')
         password2 = request.form.get('password2')
 
-        if len(twitter_tag) == 0:
+        user = User.query.filter_by(twitter_tag=twitter_tag).first()
+        if user:
+            flash('User already exists!', category='error')
+        elif len(twitter_tag) == 0:
             flash('Invalid Twitter name.', category='error')
         elif password != password2:
             flash('Passwords don\'t match.', category='error')
@@ -27,7 +31,10 @@ def sign_up():
             db.session.commit()
 
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+
+            login_user(user, remember=True)
+
+            return redirect(url_for('views.logged_in_home'))
 
     return render_template('signup.html')
 
@@ -41,8 +48,9 @@ def login():
         user = User.query.filter_by(twitter_tag=twitter_tag).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in SUccesfully!', category='success')
-                return redirect(url_for('views.home'))
+                flash('Logged in Succesfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.logged_in_home'))
             else:
                 flash('Incorrect Password, try again.', category='error')
         else:
@@ -52,5 +60,7 @@ def login():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h1>logout</h1>"
+    logout_user()
+    return redirect(url_for('auth.login'))
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required,  current_user
 
 views = Blueprint('views', __name__)
 
@@ -6,3 +7,9 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('home.html')
+
+
+@views.route('/home')
+@login_required
+def logged_in_home():
+    return render_template('logged_in_home.html')
